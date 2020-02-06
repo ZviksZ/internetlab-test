4ee5be2c476f2022d17400a2d71fbc1baa6baa27
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-02-06 10:15:39 +0300

    top block

--- app/js/common.js
+++ app/js/common.js
@@ -5,20 +5,21 @@ $(function() {
    });
 
    function toggleMenu() {
-      $('#burger__input').click(function() {
-         $('.header__wrap').toggleClass('header__open')
+      $('#burger__icon').click(function() {
+         $('.header').toggleClass('header__open')
          $('.header__menu').toggleClass('menu__hide')
       })
    }
+
    function scrollChangeMenu() {
       let scrolled;
       window.onscroll = function() {
          scrolled = window.pageYOffset || document.documentElement.scrollTop;
          if(scrolled > 50){
-            $('.header__wrap').addClass('header__open')
+            $('.header').addClass('header__scroll')
          }
          if(50 > scrolled){
-            $('.header__wrap').removeClass('header__open')
+            $('.header').removeClass('header__scroll')
          }
 
       }
